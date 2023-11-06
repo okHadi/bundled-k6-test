3564ada164aa11c9f84809e4320005ff5edcc726
Khanh <khan.hadi2951@gmail.com>
2023-11-06 22:48:30 +0500

    init commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@
 RANDOM_TAG="test_run_id=$(date +%s%N | sha256sum | base64 | head -c 16)"
 
 # Run the K6 tests in parallel with the random tag
-k6 run --out statsd --tag $RANDOM_TAG /k6/coreJourneys/journey1.js &
-k6 run --out statsd --tag $RANDOM_TAG /k6/coreJourneys/journey2.js &
+k6 run --out statsd --tag $RANDOM_TAG ./coreJourney/journey1.js &
+k6 run --out statsd --tag $RANDOM_TAG ./coreJourney/journey2.js &
 
 # Wait for both tests to finish
 wait
